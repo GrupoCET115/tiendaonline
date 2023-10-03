cb94e9f753207ac25c582ab57d4eaf04d1ee832c
Julio Cesar Mejia Rodriguez <julayrodriguez5022001@gmail.com>
2023-10-02 18:07:19 -0600

    Footer Done
    Footer and footer complements finished.

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -4,9 +4,14 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
+    path('preguntasFrecuentes/',views.QuestionFrecuentAsk, name='QuestionFrecuentAsk'),
+    path('terminosDeVenta/',views.SaleTerms, name="SaleTerms"),
+    path('terminosDeDistriucion',views.DisTerms,name="DisTerms"),
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,
          name='product_detail'),
+    
+    
 ]
