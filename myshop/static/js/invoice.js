613cc40a413ff1e1370e653198f08edcecb1c617
ale4233 <alememe45@gmail.com>
2023-11-26 06:23:58 +0000

    btcpayment completo

--- myshop/static/js/invoice.js
+++ myshop/static/js/invoice.js
@@ -2,9 +2,9 @@ var socket = new WebSocket("wss://www.blockonomics.co/payment/"+ address);
 console.log(socket)
 socket.onmessage = function(event){
   response = JSON.parse(event.data);
-  console.log("Response "+ response)
+  console.log("Response  status"+ parseInt(response.status));
   //This condition ensures that we reload only when we get a
   //new payment status and don't go into a loop
-    // if (parseInt(response.status) > parseInt(status))
+  if (parseInt(response.status) == 2){window.location.href = 'https://tdnonline.homes/payment/completed/';}
     // setTimeout(function(){window.location.reload() }, 1000);
-}
+};
